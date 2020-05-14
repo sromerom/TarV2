610efc25686f0567314b9a830ea4aaf35548a33c
Samuel Romero <sromerom@esliceu.net>
2020-05-14 20:00:39 +0200

    Implementat el metode list

--- src/Tar.java
+++ src/Tar.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Tar {
     private String fileName;
     private long size;
     private CustomFile[] files;
+    private boolean isExpanded;
     private boolean exists;
 
     // Constructor
@@ -16,11 +18,20 @@ class Tar {
         File f = new File(this.fileName);
         this.exists = f.exists();
         this.size = (int) f.length();
+        this.isExpanded = false;
     }
 
     // Torna un array amb la llista de fitxers que hi ha dins el TAR
     public String[] list() {
-        return null;
+        String[] filesName = new String[this.files.length];
+        if (this.isExpanded) {
+            int i = 0;
+            for (CustomFile cf : this.files) {
+                filesName[i] = cf.getFileName();
+                i++;
+            }
+        }
+        return filesName;
     }
 
     // Torna un array de bytes amb el contingut del fitxer que té per nom
@@ -67,7 +78,6 @@ class Tar {
                 int seguentFitxer = (int) (Math.ceil(sizeInDecimal / 512.0) * 512);
                 dis.skipBytes(seguentFitxer);
 
-                System.out.println(nameFile);
                 filesList.add(new CustomFile(nameFile, fileMode, Integer.parseInt(ownerNumberUser), Integer.parseInt(groupNumberUser), Long.parseLong(sizeFile), lastModification, Integer.parseInt(checksum), isLink, nameLinkedFile));
             }
 
@@ -77,7 +87,7 @@ class Tar {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        this.isExpanded = true;
     }
 }
 
@@ -103,5 +113,41 @@ class CustomFile {
         this.checksum = checksum;
         this.isLink = isLink;
         this.nameLinkedFile = nameLinkedFile;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFileMode() {
+        return fileMode;
+    }
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+
+    public int getGroupID() {
+        return groupID;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public String getLastModification() {
+        return lastModification;
+    }
+
+    public int getChecksum() {
+        return checksum;
+    }
+
+    public boolean isLink() {
+        return isLink;
+    }
+
+    public String getNameLinkedFile() {
+        return nameLinkedFile;
     }
 }

f9a773b55307c907a9eb5a17bad6f46eafe10739
Samuel Romero <sromerom@esliceu.net>
2020-06-03 16:16:04 +0200

    Millorant i comentant encara mes el codi

--- src/Main.java
+++ src/Main.java
@@ -11,178 +11,197 @@ public class Main {
         boolean status = true;
         if (t.isExists()) {
 
-            while (status) {
-                System.out.println("############ MENU ############");
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "1.-Load");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "2.-List");
+            if (t.isTar()) {
+                while (status) {
+                    System.out.println("############ MENU ############");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "1.-Load");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "3.-Extract");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "2.-List");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "4.-Last Modification");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "3.-Extract");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "5.-Size");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "4.-Last Modification");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "6.-UID & GID");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "5.-Size");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "7.-Permissions");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "6.-UID & GID");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "8.-Link File");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "7.-Permissions");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "9.-Help");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "8.-Link File");
 
-                System.out.printf("%11s", "");
-                System.out.printf("%-15S\n", "10.-Exit");
+                    System.out.printf("%11s", "");
+                    System.out.printf("%-15S\n", "9.-Exit");
 
-                System.out.println("##############################");
-                System.out.println();
-                System.out.print("Introduiex el numero que correspongui a la comanda que vols realitzar: ");
-                int comanda = sc.nextInt();
-                Scanner sc2;
-                switch (comanda) {
-                    //Carregar fitxer a la memoria
-                    case 1:
-                        generaEspais();
-
-                        t.expand();
-                        System.out.println("Carregat tar a la memoria...");
-                        break;
-                    //Llistam tots els fitxers
-                    case 2:
-                        if (t.isExpanded()) {
+                    System.out.println("##############################");
+                    System.out.println();
+                    System.out.print("Introduiex el numero que correspongui a la comanda que vols realitzar: ");
+                    int comanda = sc.nextInt();
+                    Scanner sc2;
+                    switch (comanda) {
+                        //Carregar fitxer a la memoria
+                        case 1:
                             generaEspais();
 
-                            String[] fitxers = t.list();
-                            for (int i = 0; i < fitxers.length; i++) {
-                                System.out.printf("%10s", "");
-                                System.out.printf("%d.-%s\n", (i + 1), fitxers[i]);
-                            }
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                    //Extreure tots els fitxers
-                    case 3:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex la ruta a on vols extreure els fitxers del Tar: (en blanc si vols extreure'ls en el directori a on estas): ");
-                            String path2 = sc2.nextLine();
-                            generaEspais();
+                            t.expand();
+                            System.out.println("Carregat tar a la memoria...");
+                            t.checksum();
+                            break;
+                        //Llistam tots els fitxers
+                        case 2:
+                            if (t.isExpanded()) {
+                                generaEspais();
 
-                            boolean aaa = t.extractTar(path2);
-                            if (aaa) {
-                                System.out.println("S'ha extret correctament");
+                                String[] fitxers = t.list();
+                                for (int i = 0; i < fitxers.length; i++) {
+                                    System.out.printf("%10s", "");
+                                    System.out.printf("%d.-%s\n", (i + 1), fitxers[i]);
+                                }
                             } else {
-                                System.out.println("No s'ha pogut extreure correctament");
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
                             }
+                            break;
+                        //Extreure tots els fitxers
+                        case 3:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex la ruta a on vols extreure els fitxers del Tar: (en blanc si vols extreure'ls en el directori a on estas): ");
+                                String path2 = sc2.nextLine();
+                                generaEspais();
 
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
+                                boolean aaa = t.extractTar(path2);
+                                if (aaa) {
+                                    System.out.println("S'ha extret correctament");
+                                } else {
+                                    System.out.println("No s'ha pogut extreure correctament");
+                                }
+
+                            } else {
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
+                            }
+                            break;
                         //Aconseguir ultima data de modificacio
-                    case 4:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex el nom del fitxer que vols comprovar l'ultima data de modificació: ");
-                            String name = sc2.nextLine();
-                            generaEspais();
+                        case 4:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex el nom del fitxer que vols comprovar l'ultima data de modificació: ");
+                                String name = sc2.nextLine();
+                                generaEspais();
 
-                            String lastModification = t.getLastModification(name);
-                            System.out.println(lastModification);
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                        //Obtenir tamany d'un fitxer en concret
-                    case 5:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex el nom del fitxer per mostrar el seu tamany: ");
-                            String name = sc2.nextLine();
-                            generaEspais();
-
-                            long size = t.getSize(name);
-                            System.out.printf("El tamany del fitxer es de %d Bytes\n", size);
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                        //Obtenir owner i group ID
-                    case 6:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex nom del fitxer per mostrar el ID del seu propietari: ");
-                            String name = sc2.nextLine();
-                            generaEspais();
-
-                            int[] ids = t.getIds(name);
-                            System.out.println("Owner ID: " + ids[0]);
-                            System.out.println("Group ID:" + ids[1]);
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                        //Obtenir permisos d'un fitxer en concret
-                    case 7:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex nom del fitxer per mostrar els seus permisos: ");
-                            String name = sc2.nextLine();
-                            generaEspais();
-
-                            int permisos = t.getPermissions(name);
-
-                            System.out.println("Els permisos del fitxer són els següents: " + permisos);
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                        //Comprovacio link
-                    case 8:
-                        if (t.isExpanded()) {
-                            sc2 = new Scanner(System.in);
-                            System.out.print("Introduiex el nom del fitxer per comprovar si es un fitxer amb link simbolic: ");
-                            String name = sc2.nextLine();
-                            generaEspais();
-
-                            int link = t.getLink(name);
-
-                            if (link != 0) {
-                                String nameFileLink = t.getNameLinkedFile(name);
-                                if (link == 1) {
-                                    System.out.println("Es un enllaç fort i correspon al fitxer " + nameFileLink);
+                                String lastModification = t.getLastModification(name);
+                                if (lastModification != null) {
+                                    System.out.println(lastModification);
+                                } else {
+                                    System.out.println("No s'ha trobat el fitxer introduit");
                                 }
-                                if (link == 2) {
-                                    System.out.println("Es un enllaç simbolic i correspon al fitxer " + nameFileLink);
+
+                            } else {
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
+                            }
+                            break;
+                        //Obtenir tamany d'un fitxer en concret
+                        case 5:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex el nom del fitxer per mostrar el seu tamany: ");
+                                String name = sc2.nextLine();
+                                generaEspais();
+
+                                long size = t.getSize(name);
+                                if (size != -1) {
+                                    System.out.printf("El tamany del fitxer es de %d Bytes\n", size);
+                                } else {
+                                    System.out.println("No s'ha trobat el fitxer introduit");
+                                }
+
+                            } else {
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
+                            }
+                            break;
+                        //Obtenir owner i group ID
+                        case 6:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex nom del fitxer per mostrar el ID del seu propietari: ");
+                                String name = sc2.nextLine();
+                                generaEspais();
+
+                                int[] ids = t.getIds(name);
+                                if (ids[0] == -1 || ids[1] == -1) {
+                                    System.out.println("No s'ha trobat el fitxer introduit");
+                                } else {
+                                    System.out.println("Owner ID: " + ids[0]);
+                                    System.out.println("Group ID:" + ids[1]);
                                 }
                             } else {
-                                System.out.println("El fitxer introduit es un fitxer normal.");
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
                             }
-                        } else {
-                            System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
-                        }
-                        break;
-                    //Un help per explicar que fa cada comanda
-                    case 9:
-                        System.out.println("Mostrant ajuda...");
-                        break;
-                    //Comanda per sortir del programa
-                    case 10:
-                        System.out.println("Sortint...");
-                        status = false;
-                        break;
+                            break;
+                        //Obtenir permisos d'un fitxer en concret
+                        case 7:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex nom del fitxer per mostrar els seus permisos: ");
+                                String name = sc2.nextLine();
+                                generaEspais();
+
+                                int permisos = t.getPermissions(name);
+                                if (permisos != -1) {
+                                    System.out.println("Els permisos del fitxer són els següents: " + permisos);
+                                } else {
+                                    System.out.println("No s'ha trobat el fitxer introduit");
+                                }
+
+                            } else {
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
+                            }
+                            break;
+                        //Comprovacio link
+                        case 8:
+                            if (t.isExpanded()) {
+                                sc2 = new Scanner(System.in);
+                                System.out.print("Introduiex el nom del fitxer per comprovar si es un fitxer amb link simbolic: ");
+                                String name = sc2.nextLine();
+                                generaEspais();
+
+                                int link = t.getLink(name);
+                                if (link == -1) {
+                                    System.out.println("No s'ha trobat el fitxer introduit");
+                                } else {
+                                    String nameFileLink = t.getNameLinkedFile(name);
+                                    if (link == 0) {
+                                        System.out.println("El fitxer introduit es un fitxer normal");
+                                    } else if (link == 1) {
+                                        System.out.println("Es un enllaç fort i correspon al fitxer " + nameFileLink);
+                                    } else if (link == 2) {
+                                        System.out.println("Es un enllaç simbolic i correspon al fitxer " + nameFileLink);
+                                    }
+
+                                }
+                            } else {
+                                System.out.println("Abans de poder fer aquesta comanda, s'ha de carregar el fitxer tar a la memoria.");
+                            }
+                            break;
+                        //Comanda que ens permet sortir del programa
+                        case 9:
+                            System.out.println("Sortint...");
+                            status = false;
+                            break;
+
+                    }
+                    System.out.println("----------------------------");
                 }
-                System.out.println("----------------------------");
+            } else {
+                System.out.println("Ho sentim, aquest programa nomes accepta arxius TAR.");
             }
 
         } else {
--- src/Tar.java
+++ src/Tar.java
@@ -9,22 +9,26 @@ class Tar {
     private long size;
     private CustomFile[] files;
     private boolean isExpanded;
+    private boolean isTar;
     private boolean exists;
 
     // Constructor
     public Tar(String filename) {
         File f = new File(filename);
 
+        //Nomes si el fitxer existeix, inicialitzarem tots els atributs de la classe Tar gràcies a les dades que ens donen els metodes de la classe File
         if (f.exists()) {
             this.pathName = f.getPath();
             this.fileName = f.getName();
             this.size = (int) f.length();
             this.isExpanded = false;
+            this.isTar = this.fileName.contains(".tar");
         }
+
         this.exists = f.exists();
     }
 
-    // Torna un array amb la llista de fitxers que hi ha dins el TAR
+    //Torna un array amb la llista de fitxers que hi ha dins el TAR
     public String[] list() {
         String[] filesName = new String[this.files.length];
         if (this.isExpanded) {
@@ -37,7 +41,7 @@ class Tar {
         return filesName;
     }
 
-    // Torna un array de bytes amb el contingut del fitxer que té per nom
+    //Torna un array de bytes amb el contingut del fitxer que té per nom
     // igual a l'String «name» que passem per paràmetre
     public byte[] getBytes(String name) {
         if (this.isExpanded) {
@@ -50,11 +54,11 @@ class Tar {
         return null;
     }
 
-    //Torna un string amb la ultima data de modificio del fitxer de dins de tar. Com a parametre s'ha d'enviar el nom del fitxer.
+    //Torna un string amb la ultima data de modificacio del fitxer. Com a parametre s'ha d'enviar el nom del fitxer.
     public String getLastModification(String name) {
         if (this.isExpanded) {
             for (CustomFile cf : this.files) {
-                if (cf.getFileName().equals(name) || cf.getFileName().contains(name)) {
+                if (cf.getFileName().equals(name)) {
                     long timestamp = Integer.parseInt(cf.getLastModification(), 8);
                     Timestamp tt = new Timestamp(timestamp * 1000);
                     Date date = new Date(tt.getTime());
@@ -65,7 +69,7 @@ class Tar {
         return null;
     }
 
-    //Retorna un long amb el tamany de un fitxer del tar en concret. Com a paramentre s'ha de passar el nom del fitxer
+    //Retorna un long amb el tamany d'un fitxer del tar en concret. Com a paramentre s'ha de passar el nom del fitxer
     public long getSize(String name) {
         for (CustomFile cf : this.files) {
             if (cf.getFileName().equals(name)) {
@@ -78,6 +82,8 @@ class Tar {
     //Retorna un array de int indicant quins son els ids de propietari i de grup d'un fitxer en concret. Com els altres, s'ha de passar per parametre un nom.
     public int[] getIds(String name) {
         int[] ids = new int[2];
+        ids[0] = -1;
+        ids[1] = -1;
         for (CustomFile cf : this.files) {
             if (cf.getFileName().equals(name)) {
                 ids[0] = cf.getOwnerID();
@@ -108,7 +114,7 @@ class Tar {
     }
 
 
-    //Metode que ens permet obtenir el name del arxiu a on hi apunta aquest link
+    //Metode que ens permet obtenir el nom del arxiu a on hi apunta un arxiu amb link
     public String getNameLinkedFile(String name) {
         for (CustomFile cf : this.files) {
             if (cf.getFileName().equals(name)) {
@@ -119,14 +125,22 @@ class Tar {
         return null;
     }
 
+
+    public void checksum() {
+        for (CustomFile cf : this.files) {
+            System.out.println(cf.getChecksum());
+        }
+    }
+
     //Metode que ens permet extreure tots els fitxers que hi ha de dins d'un fitxer tar. S'ha de passar per parametre el path a on es vol extreure el path.
-    //Aquest metode retornar un boolean indicant si el process d'extració ha sortir correctament (true) o per si al contrari no s'ha pogut fer o ha ocurregut qualsevol error(false)
+    //Aquest metode retorna un boolean indicant si el process d'extració ha sortit correctament (true) o per si al contrari no s'ha pogut fer o ha ocurregut qualsevol error(false)
     public boolean extractTar(String path) {
         File f = new File(path);
         File f2 = new File(".");
         String tarName = this.getFileName().split("\\.")[0];
         boolean createFolder = false;
 
+        //Si el path que ha introduit el usuari equival es buit, voldra dir que
         if (path.equals("") && f2.exists() && f2.isDirectory() && f2.canWrite()) {
             int i = 1;
             while (new File(tarName).exists()) {
@@ -183,11 +197,15 @@ class Tar {
             InputStream is = new FileInputStream(this.pathName);
             DataInputStream dis = new DataInputStream(is);
 
+            //Bucle per recorrer el header i el content de cada un dels fitxers que hi ha al tar
             while (true) {
                 String nameFile = new String(dis.readNBytes(100)).trim();
+
+                //En el moment que la variable nameFile sigui un string buit, voldra dir que ja es pot acabar el bucle perque no hi ha mes fitxers a recorrer
                 if (nameFile.equals("")) {
                     break;
                 }
+
                 int fileMode = Integer.parseInt(new String(dis.readNBytes(8)).trim());
                 String ownerNumberUser = new String(dis.readNBytes(8)).trim();
                 String groupNumberUser = new String(dis.readNBytes(8)).trim();
@@ -197,23 +215,32 @@ class Tar {
                 String link = new String(dis.readNBytes(1)).trim();
                 String nameLinkedFile = new String(dis.readNBytes(100)).trim();
 
+                //Avançam 255 per acabar el header i passar al content del fitxer
                 dis.skipBytes(255);
 
+                //Aconseguim el numero que ens permetra passar al següent header i obviar el bytes sobrants del content. Això passa per que els blocs van de 512 en 512
                 int sizeInDecimal = Integer.parseInt(sizeFile, 8);
                 int seguentFitxer = (int) (Math.ceil(sizeInDecimal / 512.0) * 512);
                 int addBytes = seguentFitxer - sizeInDecimal;
 
                 byte[] content = dis.readNBytes(sizeInDecimal);
+
+                //Passam el bytes que no volem i cream i guardam un objecte de tipus
                 dis.skipBytes(addBytes);
                 filesList.add(new CustomFile(nameFile, fileMode, Integer.parseInt(ownerNumberUser), Integer.parseInt(groupNumberUser), sizeInDecimal, lastModification, Integer.parseInt(checksum), Integer.parseInt(link), nameLinkedFile, content));
             }
 
+            //Inicialitzam el array a on contindra tots els fitxers del tar i passam la llista a un array
             this.files = new CustomFile[filesList.size()];
             filesList.toArray(this.files);
+
+            //Important posar l'atribut isExpanded a true, indicant que s'ha carregat a la memoria el fitxer tar.
             this.isExpanded = true;
+
+            //I no oblidar-se de tancar el fluxe de dades tant del dataInputStream com del InputStream
             dis.close();
             is.close();
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) { //Capturam les possibles excepcions
             System.out.println("No s'ha pogut trobar el tar indicat.");
             e.printStackTrace();
         } catch (IOException e) {
@@ -243,6 +270,10 @@ class Tar {
     public boolean isExists() {
         return exists;
     }
+
+    public boolean isTar() {
+        return isTar;
+    }
 }
 
 class CustomFile {
@@ -258,6 +289,7 @@ class CustomFile {
     private byte[] content; //X
 
 
+    //Classe que ens permet representar el que seria un fitxer qualsevol en un tar. Tots els seus atributs ho podem trobar en el header
     public CustomFile(String fileName, int fileMode, int ownerID, int groupID, long fileSize, String lastModification, int checksum, int link, String nameLinkedFile, byte[] content) {
         this.fileName = fileName;
         this.fileMode = fileMode;
